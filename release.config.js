f0f3a0eaac81f5defa68d5d6e536dae420b23651
Surendra <surendra@westagilelabs.com>
2023-09-17 22:08:11 +0530

    BREAKING CHANGE: remove delete api

--- release.config.js
+++ release.config.js
@@ -2,51 +2,47 @@ const config = {
   branches: ['main', { name: 'dev', prerelease: true }],
   plugins: [
     [
-      "@semantic-release/commit-analyzer",
+      '@semantic-release/commit-analyzer',
       {
-        preset: "conventionalcommits",
+        preset: 'conventionalcommits',
         releaseRules: [
-          { type: "docs", scope: "README", release: "patch" },
-          { type: "refactor", release: "patch" },
-          { type: "style", release: "patch" },
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
         ],
-        parserOpts: { noteKeywords: "BREAKING" },
+        parserOpts: { noteKeywords: 'BREAKING' },
       },
     ],
     [
-      "@semantic-release/release-notes-generator",
+      '@semantic-release/release-notes-generator',
       {
-        preset: "conventionalcommits",
-        parserOpts: { noteKeywords: "BREAKING" },
+        preset: 'conventionalcommits',
+        parserOpts: { noteKeywords: 'BREAKING' },
       },
     ],
-    // For '@semantic-release/changelog'
-    // updates the docs/CHANGELOG.md file
     [
-      "@semantic-release/changelog",
+      '@semantic-release/changelog',
       {
-        changelogTitle: "# Changelog",
+        changelogFile: 'docs/CHANGELOG.md',
       },
     ],
-    // For '@semantic-release/github',
-    // creates a release on GitHub
     [
-      "@semantic-release/github",
+      '@semantic-release/github',
       {
         successComment: false,
         releasedLabels: false,
       },
     ],
     [
-        "semantic-release-slack-bot",
-        {
-          "notifyOnSuccess": true,
-          "notifyOnFail": true,
-          "onSuccessTemplate": {
-            "text": "A new version of $package_name with version $npm_package_version has been released"
-          }
-        }
-      ]
+      'semantic-release-slack-bot',
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: true,
+        onSuccessTemplate: {
+          text: 'A new version of $package_name with version $npm_package_version has been released',
+        },
+      },
+    ],
   ],
 };
 
