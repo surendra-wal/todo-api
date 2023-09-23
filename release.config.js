72b6442638c9ae72d0d5beeb6855eefe0d3fc625
Surendra <surendra@westagilelabs.com>
2023-09-24 01:55:55 +0530

    release config changes

--- release.config.js
+++ release.config.js
@@ -1,52 +1,50 @@
 const config = {
-  branches: ['main', { name: 'dev', prerelease: true }],
+  // Define the branches where releases are allowed
+  branches: ['main', { name: 'release', prerelease: true }],
   plugins: [
     [
-      '@semantic-release/commit-analyzer',
+      '@semantic-release/commit-analyzer', // Use the commit-analyzer plugin to analyze commit messages.
       {
         preset: 'conventionalcommits',
         releaseRules: [
-          { type: 'docs', scope: 'README', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'style', release: 'patch' },
+          { type: 'docs', scope: 'README', release: 'patch' }, // For 'docs' commits with scope 'README', release a patch version.
+          { type: 'refactor', release: 'patch' }, // For 'refactor' commits, release a patch version.
+          { type: 'style', release: 'patch' }, // For 'style' commits, release a patch version.
         ],
         parserOpts: { noteKeywords: 'BREAKING' },
       },
     ],
     [
-      '@semantic-release/release-notes-generator',
+      '@semantic-release/release-notes-generator', // Use the release-notes-generator plugin to generate release notes.
       {
-        preset: 'conventionalcommits',
-        parserOpts: { noteKeywords: 'BREAKING' },
+        preset: 'conventionalcommits', // Configure it to use the conventional commits preset.
+        parserOpts: { noteKeywords: 'BREAKING' }, // Specify note keywords for breaking changes.
       },
     ],
     [
-      '@semantic-release/changelog',
+      '@semantic-release/changelog', // Use the changelog plugin to manage changelog files.
       {
-        changelogFile: 'docs/CHANGELOG.md',
+        changelogFile: 'docs/CHANGELOG.md', // Specify the location of the changelog file.
       },
     ],
     [
-      '@semantic-release/github',
+      '@semantic-release/github', // Use the GitHub plugin to interact with GitHub repositories.
       {
-        successComment: false,
-        releasedLabels: false,
+        successComment: false, // Disable posting a success comment on GitHub.
+        releasedLabels: false, // Disable updating released labels on GitHub.
       },
     ],
     [
-      '@semantic-release/git',
+      '@semantic-release/git', // Use the git plugin to commit and push changes to Git.
       {
-        assets: ['docs/CHANGELOG.md'],
+        assets: ['docs/CHANGELOG.md'], // Specify assets to be included in the Git release commit.
       },
     ],
     [
-      'semantic-release-slack-bot',
+      'semantic-release-slack-bot', // Use the semantic-release-slack-bot plugin for Slack notifications.
       {
-        notifyOnSuccess: true,
-        notifyOnFail: true,
-        onSuccessTemplate: {
-          text: 'A new version of $package_name with version $npm_package_version has been released',
-        },
+        notifyOnSuccess: true, // Notify on successful releases.
+        notifyOnFail: true, // Notify on failed releases.
       },
     ],
   ],
